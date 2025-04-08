b9320897374fe3041eba4247142ad21825ab7d50
tilothama <tilothamapasara29@gmail.com>
2025-04-08 12:14:52 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,24 @@
+import random
+possible_actions = ["rock","paper","scissors"]
+while True:
+    user_action = input("Enter a choice(rock, paper, scissors): ").lower()
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose{computer_action}.\n")
+    if user_action==computer_action:
+        print(f"Both players selected {user_action}. It's a tie")
+    elif user_action == "rock":
+        if (computer_action == "scissors"):
+            print("Rock smashes scissors! You Win!")
+        else:
+            print("paper covers rock! You lose.")
+    elif user_action == "scissors":
+        if (computer_action == "paper"):
+            print("Scissors cuts paper! You Win!")
+        else: 
+            print("Rock smashes scissors! You lose.")
+    else:
+        print("Invalid input, please enter rock, paper, or scissors.")
+
+    play_again = input("Play again?(yes/no):")
+    if play_again.lower()!="yes":
+        break 
